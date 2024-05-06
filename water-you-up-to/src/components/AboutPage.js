088e8fc6ad4599b13c4b60fb3cd2d9da266cac5a
Ken Richlin <krichlin@gmail.com>
2024-05-06 15:00:12 -0400

    Commit message

---
+++ water-you-up-to/src/components/AboutPage.js
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from "react";
+import Header from './LogForm'
+
+function AboutPage() {
+    
+    return(
+        <h2>About US!</h2>
+        <p>Water You Up To was built by Aaron Lin, Ken Richlin and Erin Yant!</p>
+        
+        <p>Scan this QR code to donate bitcoins to our project!</p>
+    )
+}
