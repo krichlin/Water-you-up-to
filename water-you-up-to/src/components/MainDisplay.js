dfcfd1ea2b4803f09e6a0769176ad240bb8b36df
Aaron Lin <aaron.l.lin.general@gmail.com>
2024-05-08 11:13:21 -0400

    fixed options

--- water-you-up-to/src/components/MainDisplay.js
+++ water-you-up-to/src/components/MainDisplay.js
@@ -1,11 +1,22 @@
 import React, {useState, useEffect} from "react";
 import ListDisplay from './ListDisplay'
 
-function MainDisplay({Logs}) {
+function MainDisplay() {
+
+  const [listItem, setListItem] = useState([])
+  
+  useEffect(() => {
+    fetch('http://localhost:6001/Logs')
+    .then((res) => res.json())
+    .then((data) => {
+        setListItem(data)
+        console.log(listItem)
+    })
+  }, [])
 
       return (
         <ul>
-            <ListDisplay logs={Logs}/>
+            <ListDisplay logs={listItem}/>
         </ul>
       )
 }
